ac98f85f3a16814bbd4f5784f37b0dd45f6ec5ae
Hussain Kapadia <hussainkapadia135@gmail.com>
2025-08-07 07:46:47 -0500

    Form update

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
 document.getElementById("clickMe").addEventListener("click", () => {
-  document.getElementById("message").textContent = "You clicked the button!";
+  document.getElementById("message").textContent =
+    "The fields have been updated!";
 });
